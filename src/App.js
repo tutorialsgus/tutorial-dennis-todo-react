6763330b002e5eff836e1d2bf062166c7fcf4a71
gustcomer <gustcomer@gmail.com>
2021-03-13 19:48:26 -0300

    Form inserido sem nenhuma formatação

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react'; 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+	render(){
+
+		return(
+			<div className="container">
+        <div id="task-container">
+          <div id="form-wrapper">
+            <form id="form">
+              <div className="flex-wrapper">
+                
+                <div style={{flex: 6}}>
+                  <input className="form-control" id="title" type="text" name="title" placeholder="Add task.." />
+                </div>
+                <div style={{flex: 1}}>
+                  <input id="submit" className="btn btn-warning" type="submit" name="Add" />
+                </div>
+
+              </div>
+
+            </form>
+          </div>
+
+          <div id="list-wrapper">
+
+          </div>
+        </div>
+			</div>
+		)
+
+	}
 }
 
 export default App;
