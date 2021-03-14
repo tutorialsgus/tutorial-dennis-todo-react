a1b8211a4d41ff82295d01ef6e38adc292efcb1e
gustcomer <gustcomer@gmail.com>
2021-03-14 08:15:41 -0300

    fetch data ok mas apenas joga no console.log

--- src/App.js
+++ src/App.js
@@ -3,6 +3,35 @@ import './App.css';
 
 
 class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      todoList:[],
+      activeItem:{
+        id:null, 
+        title:'',
+        completed:false,
+      },
+      editing:false,
+    }
+    this.fetchTasks = this.fetchTasks.bind(this)
+  };
+
+  componentWillMount(){
+    this.fetchTasks()
+  }
+
+  fetchTasks(){
+    console.log('Fetching...')
+
+    fetch('http://127.0.0.1:8000/api/task-list/')
+    .then(response => response.json())
+    .then(data => 
+      console.log('Data:', data)
+    )
+  }
+
 	render(){
 
 		return(
@@ -11,7 +40,7 @@ class App extends React.Component {
           <div id="form-wrapper">
             <form id="form">
               <div className="flex-wrapper">
-                
+
                 <div style={{flex: 6}}>
                   <input className="form-control" id="title" type="text" name="title" placeholder="Add task.." />
                 </div>
